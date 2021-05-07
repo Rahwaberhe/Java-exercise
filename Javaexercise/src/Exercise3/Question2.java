ac6617dff6590830d7e378bca25271a0e0903664
Rahwaberhe <habobeymearey@gmail.com>
2021-05-06 20:22:52 -0400

    fromexercise3Q2

--- Javaexercise/src/Exercise3/Question2.java
+++ Javaexercise/src/Exercise3/Question2.java
@@ -3,13 +3,12 @@ package Exercise3;
 public class Question2 {
 
 	public static void main(String[] args) {
-		
+	for(int j=0; j<4; j++) {
+		System.out.println("**********");
 	
-		for (int i = 0; i < 4; i++) {
-			
-			System.out.println("**********");
+	}
+	   
 
 	}
 
-}
 }
