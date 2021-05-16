bbe3f60d48268d088d3caa34f7f3ee15b5ed0013
Rahwaberhe <habobeymearey@gmail.com>
2021-05-16 19:16:44 -0400

    Exam1Q1

---
+++ Javaexercise/src/ExamQ1/Question1.java
@@ -0,0 +1,16 @@
+package ExamQ1;
+
+public class Question1 {
+
+	public static void main(String[] args) {
+		String name = "rahwa";
+		String city_and_state = "charlotte nc";
+		double  grosssalary = 100000;
+		double tax = 0.2*grosssalary;
+	    double  netsalary = grosssalary-tax;
+     System.out.println("hello my name is : " + name+"," + "I live in " + city_and_state +","+"my gross salary is" + "$"+grosssalary+ "I pay"+"$"+tax+"in tax"+ "and i take home" + "$"+netsalary );                
+     
+     
+	}
+
+}
