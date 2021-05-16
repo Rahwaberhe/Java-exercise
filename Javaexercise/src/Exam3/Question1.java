3a1619c52096a2f1ce91c2c632c43c5d5e2ba0d7
Rahwaberhe <habobeymearey@gmail.com>
2021-05-16 19:13:35 -0400

    exam3Q1

---
+++ Javaexercise/src/Exam3/Question1.java
@@ -0,0 +1,23 @@
+package Exam3;
+
+public class Question1 {
+
+	public static void main(String[] args) {
+		for(int i = 0; i<=5; i++) {
+		 for(int j = 0; j<=5; j++) {
+		 System.out.print("");
+		 }
+		for(int k = 0; k<i; k++) {
+			 System.out.print("*");
+		}
+		 System.out.println();	
+		 }
+		
+		 
+		 }
+	}
+
+
+	
+
+
